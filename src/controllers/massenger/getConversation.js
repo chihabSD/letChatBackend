71751f9ab4428f3c8bb8af841746ea033b940fca
chihabeddine <chihabeddine@chihabeddines-MacBook-Pro.local>
2023-02-21 19:53:25 +0000

    ui update

--- src/controllers/massenger/getConversation.js
+++ src/controllers/massenger/getConversation.js
@@ -1,10 +1,35 @@
 const Conversation = require("../../models/conversation");
+const Message = require("../../models/message");
 
 const getConversation = async (req, res) => {
   const senderId = req.user.user._id;
   try {
-    const chats = await Conversation.find({})
-     return res.status(200).send({ chats});
+
+    // load all conversaation for current user 
+    const chats = await Conversation.find({}).populate("users");
+    
+    // get all messages associated with the conversation Id
+    let messagesForFirstConversation = await Message.find({conversationId:chats[0]._id})
+    let messages = await Message.find({conversationId:chats[0]._id}).populate('conversationId').populate('senderId').populate('receiverId');
+    // let messages = await Message.find({senderId}).populate('conversationId').populate('senderId').populate('receiverId');
+    // let receiverId  = await Message.find({senderId}).populate('conversationId').populate('senderId').populate('receiverId');
+ 
+    // find message for current user
+    // get all message for chat
+
+      let sortedMessages = []
+      messages.find(message => {
+        if(message.conversationId.equals(chats[0]._id)) {
+
+          sortedMessages.push(message) 
+        }
+      })
+  
+
+      console.log(messagesForFirstConversation.length);
+
+   
+    return res.status(200).send({ chats, messages });
   } catch (e) {
     console.log(e);
   }
--- src/controllers/massenger/getMessage.js
+++ src/controllers/massenger/getMessage.js
@@ -4,12 +4,13 @@ const getMessage = async (req, res) => {
   const conversationId = req.params.conversationId;
   try {
     let messages = await Message.find({conversationId}).populate('conversationId').populate('senderId').populate('receiverId');
+
     // getAllMessages = getAllMessages.filter(
     //   (message) =>
     //     (message.senderId == senderId && message.receiverId == fdId) ||
     //     (message.receiverId == senderId && message.senderId == fdId)
     // );
-
+console.log(messages.length);
     return res.status(200).send({ messages });
   } catch (e) {
     console.log(e);
